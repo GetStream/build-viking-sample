da59c01703f2398e25facbf092630f1e9a736263
Neevash Ramdial <mail@neevash.dev>
2020-11-15 21:53:16 -0400

    add util function to format date

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,2 +1,12 @@
+import 'package:flutter/material.dart';
+
 export './extensions.dart';
 export './providers.dart';
+
+String formatDate(BuildContext context, DateTime time) {
+  if (time == null) {
+    return "Never";
+  } else {
+    return MaterialLocalizations.of(context).formatMediumDate(time);
+  }
+}
